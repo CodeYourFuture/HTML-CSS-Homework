e4d9ac736b64bafeaaf65114ea455bbe6d9fc297
Sadat <sadat0akhtar@gmail.com>
2020-05-27 19:45:05 +0100

    adjusted the slider navigation panel

--- week3/2-website/app.js
+++ week3/2-website/app.js
@@ -8,15 +8,15 @@ const navLinks = document.querySelectorAll('.navlinks li');
 //event listeners
 
 //toggle the nav burger
-burger.addEventListener('click', () =>{
+burger.addEventListener('click' , () => {
     nav.classList.toggle('nav-active');
 
     //animate the links
-    navLinks.forEach((links, index) =>{
-            if(List.style.animation){
-                list.style.animation = ""
+    navLinks.forEach((link, index) =>{
+            if(link.style.animation){
+                link.style.animation = ''
             }else{
-                list.style.animation = `navlinksfade 0.9s ease forwards ${index / 7+ 1}s`
+                link.style.animation = `navLinkFade 0.9s ease forwards ${index / 7 + 1}s`
             }
     });
 });
